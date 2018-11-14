d4198959c5014344cbc1572ba4dca0732f5339bb
Achille Heraud <achille@heraud.xyz>
2018-11-14 09:01:44 -0600

    Added win_amd64 wheel

--- setup.py
+++ setup.py
@@ -15,6 +15,9 @@ def build_native(spec):
 
 setup(
 	name = 'tetris_learning_environment',
+	author = 'Achille Heraud',
+	url = 'https://github.com/aHeraud/tetris-learning-environment-python',
+	license = "MIT",
 	version = '0.0.1',
 	packages = find_packages(),
 	include_package_data = True,
