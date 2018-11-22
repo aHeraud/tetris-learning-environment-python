e67ee0b5edd49d6545b7328de748903b6526d2b4
Atlas <achille@heraud.xyz>
2018-11-21 23:16:52 -0600

    The numpy ndarray returned by get_pixels is now a 2d array (dimensions are 160 x 144).

--- tetris_learning_environment/__init__.py
+++ tetris_learning_environment/__init__.py
@@ -44,6 +44,15 @@ class Environment:
 		return lib.get_score(self.__obj)
 
 	def get_pixels(self) -> numpy.ndarray:
-		# this still needs to be tested
+		"""
+		Returns the contents of the screen as a 160x144 array of RGBA pixel values,
+		where each component (R,G,B,A) is 8-bits.
+		
+		The alpha component is always 255.
+		"""
 		buffer = ffi.buffer(lib.get_pixels(self.__obj), self.WIDTH * self.HEIGHT * 4)
-		return numpy.frombuffer(buffer, dtype=numpy.uint32, count=self.WIDTH * self.HEIGHT)
+		return numpy.ndarray((self.HEIGHT, self.WIDTH),
+		                      dtype=numpy.uint32,
+		                      buffer=buffer,
+		                      offset=0,
+		                      strides=(self.WIDTH * 4, 4))
